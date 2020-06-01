68aa5daf6fe5574c2d2f85d8b43583d4b1d3a255
Mustangg <61333062+Mustangg@users.noreply.github.com>
2020-06-01 14:25:27 +0400

    Add files via upload

---
+++ qwer/qwer/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace qwer
+{
+    public partial class Form1 : Form
+    {
+        public string CmdText = "SELECT * FROM [Doljnost]";
+        public string ConnString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\\Users\\пользователь\\Desktop\\лабы\\123.accdb";
+        public Form1()
+        {
+            InitializeComponent();
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(CmdText, ConnString);
+            DataSet ds = new DataSet();
+            dataAdapter.Fill(ds, "[Doljnost]");
+            dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+        }
+    }
+}
